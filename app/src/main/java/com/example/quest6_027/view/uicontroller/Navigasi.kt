c8080f3acf0a7060253b58943a806397f5c8a03d
Kevin Adrian <kevin19305.ib@gmail.com>
2025-11-21 16:08:05 +0700

    mengubah dan menambahkan statusui pada Navigasi.kt

---
+++ app/src/main/java/com/example/quest6_027/view/uicontroller/Navigasi.kt
@@ -0,0 +1,64 @@
+package com.example.quest6_027.view.uicontroller
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.quest6_027.model.DataJK.JenisK
+import com.example.quest6_027.view.FormIsian
+import com.example.quest6_027.view.TampilanData
+import com.example.quest6_027.viewmodel.SiswaViewModel
+
+enum class Navigasi {
+    Formulir,
+    Detail
+}
+
+@Composable
+fun DataApp(
+    modifier: Modifier = Modifier,
+    viewModel: SiswaViewModel = viewModel (),
+    navController: NavHostController = rememberNavController()
+){
+    Scaffold { isiRuang ->
+
+        val uiState = viewModel.statusUI.collectAsState()
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulir.name,
+            modifier = Modifier.padding(isiRuang)
+        ) {
+            composable(route = Navigasi.Formulir.name) {
+                val konteks = LocalContext.current
+                FormIsian (
+                    pilihanK =JenisK.map {id -> konteks.resources.getString(id)},
+                    OnSubmitButtonClicked = {
+                        viewModel.setSiswa(it)
+                        navController.navigate(Navigasi.Detail.name)
+                    }
+                )
+            }
+
+            composable(route = Navigasi.Detail.name) {
+                TampilanData (
+                    statusUiSiswa = uiState.value,
+                    onBackButonClicked = {
+                        cancelAndBackToFormulir(navController)
+                    }
+                )
+            }
+        }
+    }
+}
+
+private fun cancelAndBackToFormulir(navController: NavHostController
+) {
+    navController.popBackStack(Navigasi.Formulir.name, inclusive = false)
+}
